f03bb35538c5fecb37f1f7994e287c4dda4edc43
Bert Verschoren <bverschoren@gmail.com>
2025-06-18 23:16:28 +0200

    Updated to search for int in mongoDB

--- mongo_utils.py
+++ mongo_utils.py
@@ -5,5 +5,6 @@ def get_db_connection(uri, db_name):
     return client[db_name]
 
 def fetch_activity_by_strava_id(db, strava_id):
-    return db.stravaactivities.find_one({"stravaId": str(strava_id)})
-
+    # Ensure we are comparing using an integer
+    strava_id = int(strava_id)
+    return db.stravaactivities.find_one({"stravaId": strava_id})
